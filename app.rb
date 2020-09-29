d947be7d43eacb831cde6d6b66131786d45d750f
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-29 04:25:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,6 +20,7 @@ module FormsLab
          ship.save
        end 
       
+      redirect to "/pirates/#{pirate.id}"
     #   pirate = params[:pirate]
     #   @pirate = Pirate.new(pirate[:name], pirate[:weight], pirate[:height])
     #   params[:pirate][:ships].each do |ship| 
